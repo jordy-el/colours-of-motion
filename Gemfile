25be6f07d7978c417ba4c2928fdea6ec6636d102
Jordan Ellis-Lynch <jordan_nose2@hotmail.com>
2017-05-22 14:48:46 +1000

    Clean up CLI

--- Gemfile
+++ Gemfile
@@ -1,5 +1,4 @@
 source 'https://rubygems.org'
 
-gem 'streamio-ffmpeg'
 gem 'mini_magick'
 gem 'ruby-progressbar'
--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
-require 'streamio-ffmpeg'
+require_relative './streamio/lib/streamio-ffmpeg.rb'
 require 'fileutils'
 require 'mini_magick'
+require 'ruby-progressbar'
 
 if !ARGV[0]
   puts "No input file specified."
@@ -8,7 +9,7 @@ if !ARGV[0]
 end
 
 MOVIE_PATH = ARGV[0]
-OUTPUT_PATH = MOVIE_PATH.gsub("mp4", "jpg")
+OUTPUT_PATH = "#{MOVIE_PATH}.jpg"
 
 begin
   video = FFMPEG::Movie.new(MOVIE_PATH)
@@ -22,36 +23,41 @@ choice = STDIN.gets.chomp.downcase
 puts "\n"
 
 if choice[0] == 'y'
-  begin
-    # Should turn mp4 into separate keyframes and store them in a buffer
-    video.transcode("buffer/%10d.jpg", %w(-vf select='eq(pict_type\,I)' -vsync vfr))
-  rescue
-    image_buffer = Dir.glob("buffer/*")
-    puts "Done creating thumbnails...\n\n"
+  #
+  # Should turn mp4 into separate keyframes and store them in a buffer
+  video_progress_bar = ProgressBar.create(format: "%t: |%B| %p%%", title: "Processing video", total: 100)
+  video.transcode("buffer/%10d.jpg", %w(-vf select='eq(pict_type\,I)' -vsync vfr), validate: false) do |progress|
+    video_progress_bar.progress =  (progress * 100)
+  end
+  image_buffer = Dir.glob("buffer/*")
 
-    # Should resize picture to single pixel of average colour in frame
-    image_buffer.each do |file|
-      MiniMagick::Tool::Convert.new do |convert|
-        convert << file
-        convert << "-resize" << "1x1!"
-        convert << file
-      end
-    end
-
-    # Should stitch all of the frames together, then resize
+  #
+  # Should resize picture to single pixel of average colour in frame
+  average_progress_bar = ProgressBar.create(format: "%t: |%B| %p%%", title: "Getting averages", total: image_buffer.length)
+  image_buffer.each do |file|
     MiniMagick::Tool::Convert.new do |convert|
-      convert << "+append"
-      convert << "buffer/*.jpg"
-      convert << "#{OUTPUT_PATH}"
+      convert << file
+      convert << "-resize" << "1x1!"
+      convert << file
     end
-    MiniMagick::Tool::Convert.new do |convert|
-      convert << "#{OUTPUT_PATH}"
-      convert << "-scale" << "3240x1080!"
-      convert << "#{OUTPUT_PATH}"
-    end
+    average_progress_bar.increment
   end
 
+  #
+  # Should stitch all of the frames together, then resize
+  MiniMagick::Tool::Convert.new do |convert|
+    convert << "+append"
+    convert << "buffer/*.jpg"
+    convert << "#{OUTPUT_PATH}"
+  end
+  MiniMagick::Tool::Convert.new do |convert|
+    convert << "#{OUTPUT_PATH}"
+    convert << "-scale" << "3240x1080!"
+    convert << "#{OUTPUT_PATH}"
+  end
+
+  #
   # Should empty image buffer
   image_buffer.each { |file| FileUtils.rm(file) }
-  puts "Done."
+  puts "\nDone.\n\n"
 end
