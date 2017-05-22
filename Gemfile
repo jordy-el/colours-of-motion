4fffa9f24c1c5ab2b0c9d95f0f33c510547c3895
Jordan Ellis-Lynch <jordan_nose2@hotmail.com>
2017-05-22 12:12:38 +1000

    Optimize minimagick workflow

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source 'https://rubygems.org'
 
 gem 'streamio-ffmpeg'
 gem 'mini_magick'
+gem 'ruby-progressbar'
--- app.rb
+++ app.rb
@@ -13,44 +13,45 @@ OUTPUT_PATH = MOVIE_PATH.gsub("mp4", "jpg")
 begin
   video = FFMPEG::Movie.new(MOVIE_PATH)
 rescue SystemCallError
-  puts "File does not exist."
+  puts "\nFile does not exist."
   abort
 end
 
-print "Are you sure you want to convert '#{MOVIE_PATH}'? [y/n]: "
+print "\nAre you sure you want to convert '#{MOVIE_PATH}'? [y/n]: "
 choice = STDIN.gets.chomp.downcase
-puts "\n\n"
+puts "\n"
 
 if choice[0] == 'y'
   begin
-    video.transcode("buffer/%10d.jpg", %w(-loglevel quiet -hide_banner -vf fps=1 scale=100:-1))
-    # video.transcode("buffer/%d.jpg", %w(-loglevel quiet -hide_banner -vframes 1))
+    # Should turn mp4 into separate keyframes and store them in a buffer
+    video.transcode("buffer/%10d.jpg", %w(-vf select='eq(pict_type\,I)' -vsync vfr))
   rescue
     image_buffer = Dir.glob("buffer/*")
     puts "Done creating thumbnails...\n\n"
-    averages = {}
+
+    # Should resize picture to single pixel of average colour in frame
     image_buffer.each do |file|
-      image = MiniMagick::Image.open(file)
-      image.resize("1x1")
-      average_colour = image.get_pixels.flatten
-      puts "#{file}: Average done, #{average_colour}"
-      averages[file] = average_colour
-    end
-    averages.each do |key, value|
       MiniMagick::Tool::Convert.new do |convert|
-        puts "#{key}: Conversion done"
-        convert << "#{key}"
-        convert << "-alpha" << "off" << "-fill" << "rgb#{value}".gsub("[", "(").gsub("]", ")") << "-colorize" << "100%"
-        convert << "-resize" << "1x100\!"
-        convert << "#{key}"
+        convert << file
+        convert << "-resize" << "1x1!"
+        convert << file
       end
     end
+
+    # Should stitch all of the frames together, then resize
     MiniMagick::Tool::Convert.new do |convert|
       convert << "+append"
       convert << "buffer/*.jpg"
       convert << "#{OUTPUT_PATH}"
     end
+    MiniMagick::Tool::Convert.new do |convert|
+      convert << "#{OUTPUT_PATH}"
+      convert << "-scale" << "3240x1080!"
+      convert << "#{OUTPUT_PATH}"
+    end
   end
+
+  # Should empty image buffer
   image_buffer.each { |file| FileUtils.rm(file) }
   puts "Done."
 end
